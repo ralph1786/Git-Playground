4ae3c2b3ff9057cda6ee39daa7288eaabba6d2d0
ralph1786 <ralph1786@gmail.com>
2020-07-22 15:19:24 -0400

    added func call

--- JS/indextwo.js
+++ JS/indextwo.js
@@ -10,10 +10,12 @@ objOne["name"] = "Tracey";
 // console.log(newObj === objOne);
 console.log(newObj);
 
-const arrayNums = [2, 4, 5, 3, 54, 64, 6, 4, 4];
+const arrayNums = [2, 4, 5, 3, 54, 64, 6, 4, 4, 79];
 
 function findMaxNum(array) {
   let max;
   max = Math.max(...array);
   console.log(max);
 }
+
+findMaxNum(arrayNums);
